a8748d6a3fdd10073f2d359cf332bfae0af9a858
Michal Kubišta <michal.kubista@nielsen.com>
2019-04-25 22:12:12 +0200

    Added support for headless driver.

--- process_reviews.py
+++ process_reviews.py
@@ -1,13 +1,21 @@
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import app_reviews as ap
 
-import app_reviews as apr
+DEBUG = True
+
+if not DEBUG:
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+else:
+    options = Options()
 
 # EXAMPLE RUN - too short to be usefull
-whatsap = apr.app_reviews(
-    webdriver.Chrome('chromedriver.exe'),
-    "https://play.google.com/store/apps/details?id=com.whatsapp&showAllReviews=true"
+whatsap = ap.app_reviews(
+    webdriver.Chrome('chromedriver.exe', options = options),
+    "https://play.google.com/store/apps/details?id=com.facebook.Socal&showAllReviews=true"
     )
-whatsap.run_it(max_iter = 20, rate = 0.8)
-whatsap.unwrap_reviews()
+whatsap.run_it(max_iter = 10, rate = 0.7)
 result = whatsap.collect_data()
